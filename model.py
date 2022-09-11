84944f19f493e68efe58f1cd841e28797f19979b
burakcan-izmirli <burakcanizmirli@gmail.com>
2022-09-11 23:00:01 +0300

    first trainable version

--- model.py
+++ model.py
@@ -10,10 +10,12 @@ warnings.filterwarnings('ignore')
 # %%
 dataset_raw = pd.read_pickle("burakcan_dataset.pkl")
 
+dataset_raw = dataset_raw.sample(frac=1).reset_index(drop=True)
+
 plt.hist(dataset_raw.pic50, bins = 50)
 plt.show()
 
-dataset_raw = dataset_raw.iloc[0:1000]
+dataset_raw = dataset_raw.iloc[0:5000]
 
 # %%
 permuted_indices = np.random.permutation(np.arange(dataset_raw.shape[0]))
@@ -53,6 +55,7 @@ x_train_conv = convert_conv_dataset(x_train_conv)
 x_valid_conv = convert_conv_dataset(x_valid_conv)
 x_test_conv = convert_conv_dataset(x_test_conv)
 
+
 # %%
 def merged_model(
         atom_dim,
@@ -80,21 +83,27 @@ def merged_model(
     # x = layers.Dense(1, activation="sigmoid")(x)
 
     input_layer = layers.Input(shape = (16501, 4, 1), name = 'input_layer')
-    conv_1 = layers.Conv2D(64, (1, 4), strides = 1, padding = 'same', activation = 'relu', name = 'conv_1')(input_layer)
-    conv_2 = layers.Conv2D(64, (1, 4), strides = 1, padding = 'same', activation = 'relu', name = 'conv_2')(conv_1)
-    dropout_1 = layers.Dropout(0.5, name = 'dropout_1')(conv_2)
-    pooling_1 = layers.MaxPooling2D((1, 1), name = 'pooling_1')(dropout_1)
-    conv_3 = layers.Conv2D(64, (1, 1), activation = 'relu', name = 'conv_3')(pooling_1)
-    pooling_2 = layers.MaxPooling2D((2, 2), name = 'pooling_2')(conv_3)
+    conv_1 = layers.Conv2D(1, (4, 1), strides = 1, padding = 'same', activation = 'relu', name = 'conv_1')(input_layer)
+    batch_norm_1 = layers.BatchNormalization()(conv_1)
+    conv_2 = layers.Conv2D(1, (4, 1), strides = 1, padding = 'same', activation = 'relu', name = 'conv_2')(batch_norm_1)
+    batch_norm_2 = layers.BatchNormalization()(conv_2)
+    dropout_1 = layers.Dropout(0.1, name = 'dropout_1')(batch_norm_2)
+    pooling_1 = layers.MaxPooling2D((2, 2), name = 'pooling_1')(dropout_1)
+    conv_3 = layers.Conv2D(1, (1, 1), activation = 'relu', name = 'conv_3')(pooling_1)
+    batch_norm_3 = layers.BatchNormalization()(conv_3)
+    pooling_2 = layers.MaxPooling2D((2, 2), name = 'pooling_2')(batch_norm_3)
     flatten_1 = layers.Flatten()(pooling_2)
 
     concat = layers.concatenate([flatten_1, x], name = 'concat')
 
-    dense_1 = layers.Dense(1, activation = 'relu')(concat)
+    dense_1 = layers.Dense(256, activation = 'relu')(concat)
+    dense_2 = layers.Dense(128, activation = 'relu')(dense_1)
+    dense_3 = layers.Dense(1, activation = 'linear')(dense_2)
+
 
     model = keras.Model(
         inputs = [input_layer, atom_features, bond_features, pair_indices, molecule_indicator],
-        outputs = [dense_1],
+        outputs = [dense_3],
         name = 'final_output'
     )
 
@@ -103,27 +112,27 @@ def merged_model(
 
 # %%
 model = merged_model(
-    atom_dim = x_train_mpnn[0][0][0].shape[0], bond_dim = x_train_mpnn[1][0][0].shape[0])
+    atom_dim = x_train_mpnn[0][0][0].shape[0],
+    bond_dim = x_train_mpnn[1][0][0].shape[0])
+
 # %%
 model.compile(
     loss = keras.losses.MeanSquaredError(),
-    optimizer = keras.optimizers.Adam(lr = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = None, decay = 0.0,
-                                      amsgrad = False)
+    optimizer = keras.optimizers.Adam(learning_rate=0.01)
     # metrics=[keras.metrics.AUC(name="AUC")],
 )
 
 keras.utils.plot_model(model, show_dtype = True, show_shapes = True)
 
 # %%
-train_dataset = dataset_new(x_train_conv, x_train_mpnn, y_train, batch_size = 25)
-valid_dataset = dataset_new(x_valid_conv, x_valid_mpnn, y_valid, batch_size = 25)
+train_dataset = dataset_new(x_train_conv, x_train_mpnn, y_train, batch_size = 32)
+valid_dataset = dataset_new(x_valid_conv, x_valid_mpnn, y_valid, batch_size = 32)
 
 # %%
 history = model.fit(
     train_dataset,
     validation_data = valid_dataset,
-    epochs = 50,
+    epochs = 10,
     verbose = 1
 )
 
-# %%
