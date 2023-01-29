90e2fbfae3332ffd517c3c95ae3a674139474bd2
burakcan-izmirli <burakcanizmirli@gmail.com>
2023-01-29 23:00:09 +0300

    updating dataset paths

--- src/util/static/data_type.py
+++ src/util/static/data_type.py
@@ -6,8 +6,8 @@ class DataType(Enum):
         self.label = label
         self.path = path
 
-    normal = 'normal', '../../data/processed/dataset.pkl'
-    l1000 = 'l1000', '../../data/processed/dataset_l1000.pkl'
-    pathway = 'pathway', '../../data/processed/dataset_pathway_sorted.pkl'
-    pathway_reduced = 'pathway_reduced', '../../data/processed/dataset_pathway_sorted_reduced.pkl'
-    digestive = 'digestive', '../../data/processed/dataset_tissue_digestive_system.pkl'
+    normal = 'normal', './data/processed/dataset.pkl'
+    l1000 = 'l1000', './data/processed/dataset_l1000.pkl'
+    pathway = 'pathway', './data/processed/dataset_pathway_sorted.pkl'
+    pathway_reduced = 'pathway_reduced', './data/processed/dataset_pathway_sorted_reduced.pkl'
+    digestive = 'digestive', './data/processed/dataset_tissue_digestive_system.pkl'
