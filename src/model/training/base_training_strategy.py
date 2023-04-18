a1089754654caa04dbd5a31238c1a8ce4428f032
burakcan-izmirli <burakcanizmirli@gmail.com>
2023-04-18 17:23:15 +0300

    feat(DRS-4): refactoring base training strategy

--- src/model/training/base_training_strategy.py
+++ src/model/training/base_training_strategy.py
@@ -1,9 +1,9 @@
-""" Base model strategy """
+""" Base training strategy """
 from abc import ABC, abstractmethod
 
 
-class BaseModelStrategy(ABC):
-    """ Base model strategy """
+class BaseTrainingStrategy(ABC):
+    """ Base training strategy """
 
     @abstractmethod
     def train_and_evaluate_model(self, model_creation_strategy, dataset_tuple, batch_size, learning_rate, epoch):
