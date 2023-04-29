2ebd393571ccdfc9f6a45941667790981c6b738c
burakcan-izmirli <burakcanizmirli@gmail.com>
2023-04-29 16:53:14 +0300

    fix(DSE-6): changing structure of stratified dataset strategies

--- src/dataset/drug_cell_stratified_dataset_strategy.py
+++ src/dataset/drug_cell_stratified_dataset_strategy.py
@@ -12,10 +12,6 @@ class DrugCellStratifiedDatasetStrategy(BaseDatasetStrategy):
 
     def create_splitter(self, dataset, random_state):
         """ Create splitter """
-        pass
-
-    def split_dataset(self, dataset, *args, **kwargs):
-        """ Split dataset """
 
         grouped_by_drug_df = dataset.drop_duplicates(subset=['drug_name'], keep='first')
         grouped_by_drug_df = grouped_by_drug_df[['drug_name']]
@@ -37,7 +33,38 @@ class DrugCellStratifiedDatasetStrategy(BaseDatasetStrategy):
         for group in range(1, NSplit.stratified.value + 1):
             train_df = dataset.query('drug_group!=@group & cell_group!=@group')
             test_df = dataset.query('drug_group==@group | cell_group==@group')
+            yield train_df, test_df
+
+    def split_dataset(self, dataset, *args, **kwargs):
+        """ Split dataset """
+        return dataset[['drug_name', 'cell_line_name']], dataset[['pic50']]
 
     def prepare_dataset(self, dataset, split_type, batch_size, random_state):
-        """ Prepare dataset """
-        pass
+        """
+        Main function for preparing dataset
+        :param dataset: Dataset
+        :param split_type: Split type [random, cell_stratified, drug_stratified, cell_drug_stratified]
+        :param batch_size: Batch size
+        :param random_state: Random state
+        :return: atom_dim, bond_dim, train_dataset, valid_dataset, test_dataset
+        """
+        mpnn_dataset, conv_dataset = self.create_mpnn_and_conv_dataset(dataset)
+
+        dataset = dataset[['drug_name', 'cell_line_name', 'pic50']]
+        splitter = self.create_splitter(dataset, random_state)
+        for train_df, test_df in splitter:
+            x_train, y_train = self.split_dataset(train_df)
+            # Creating Tensorflow datasets
+            atom_dim, bond_dim, cell_line_dim, train_dataset = self.tf_dataset_creator(x_train, y_train, batch_size,
+                                                                                       mpnn_dataset, conv_dataset)
+            del train_df, x_train, y_train
+
+            x_test, y_test = self.split_dataset(test_df)
+            # Creating Tensorflow datasets
+            atom_dim_test, bond_dim_test, cell_line_dim_test, test_dataset = self.tf_dataset_creator(x_test, y_test,
+                                                                                                     batch_size,
+                                                                                                     mpnn_dataset,
+                                                                                                     conv_dataset)
+            del test_df, x_test
+
+            yield (atom_dim, bond_dim, cell_line_dim), train_dataset, test_dataset, y_test
