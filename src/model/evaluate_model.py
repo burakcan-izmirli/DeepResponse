79d1cc448b89a6c55142ae4890f16a125e717ea2
burakcan-izmirli <burakcanizmirli@gmail.com>
2023-11-19 19:41:57 +0300

    feat(DRS-15): flatten the arrays for pearson

--- src/model/evaluate_model.py
+++ src/model/evaluate_model.py
@@ -1,7 +1,7 @@
 """ Evaluate model """
-
+import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, precision_score, recall_score, \
-    accuracy_score, f1_score, matthews_corrcoef, auc
+    accuracy_score, f1_score, matthews_corrcoef
 from sklearn.preprocessing import Binarizer
 from scipy.stats import pearsonr, spearmanr
 
@@ -25,15 +25,18 @@ def evaluate_model(y_test, y_pred):
     :param y_pred: y_pred
     :return: Evaluation metrics
     """
+
+    np.save("y_test.npy", y_test)
+    np.save("y_pred.npy", y_pred)
+
     return {"Mean Squared Error": mean_squared_error(y_test, y_pred),
             "Mean Absolute Error": mean_absolute_error(y_test, y_pred),
             "Root Mean Squared Error": mean_squared_error(y_test, y_pred, squared=False),
             "R2 Score": r2_score(y_test, y_pred),
-            "Pearson": pearsonr(y_test, y_pred),
+            "Pearson": pearsonr(y_test.flatten(), y_pred.flatten()),
             "Spearman": spearmanr(y_test, y_pred),
             "Accuracy": accuracy_score(binarize_data(y_test), binarize_data(y_pred)),
             "Precision": precision_score(binarize_data(y_test), binarize_data(y_pred)),
             "Recall": recall_score(binarize_data(y_test), binarize_data(y_pred)),
             "F1 Score": f1_score(binarize_data(y_test), binarize_data(y_pred)),
             "Matthew's Correlation Coef": matthews_corrcoef(binarize_data(y_test), binarize_data(y_pred))}
-
