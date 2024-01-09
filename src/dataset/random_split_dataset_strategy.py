5324764665bc976b8bed991afaf6ef171fa708e8
burakcan-izmirli <burakcanizmirli@gmail.com>
2024-01-09 23:38:10 +0300

    removing head

--- src/dataset/random_split_dataset_strategy.py
+++ src/dataset/random_split_dataset_strategy.py
@@ -55,7 +55,7 @@ class RandomSplitDatasetStrategy(BaseDatasetStrategy):
         :param random_state: Random state
         :return: Tuple containing atom_dim, bond_dim, cell_line_dim, train_datasets, valid_datasets, test_datasets, y_test
         """
-        dataset = dataset['dataset'].head(1000)
+        dataset = dataset['dataset']
         mpnn_dataset, conv_dataset = self.create_mpnn_and_conv_dataset(dataset)
         dataset = dataset[['drug_name', 'cell_line_name', 'pic50']]
 
