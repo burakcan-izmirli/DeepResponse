36b29b35f47adcea9650b9ce503ed4d1eef0fc82
burakcan-izmirli <burakcanizmirli@gmail.com>
2024-01-12 16:53:40 +0300

    custom r2 score

--- src/dataset/random_split_dataset_strategy.py
+++ src/dataset/random_split_dataset_strategy.py
@@ -55,7 +55,7 @@ class RandomSplitDatasetStrategy(BaseDatasetStrategy):
         :param random_state: Random state
         :return: Tuple containing atom_dim, bond_dim, cell_line_dim, train_datasets, valid_datasets, test_datasets, y_test
         """
-        dataset = dataset['dataset']
+        dataset = dataset['dataset'].head(10)
         mpnn_dataset, conv_dataset = self.create_mpnn_and_conv_dataset(dataset)
         dataset = dataset[['drug_name', 'cell_line_name', 'pic50']]
 
--- src/model/training/random_split_training_strategy.py
+++ src/model/training/random_split_training_strategy.py
@@ -1,11 +1,18 @@
 """ Random split training strategy """
 import logging
+import tensorflow as tf
 from tensorflow import keras
 from sklearn.metrics import r2_score
 
 from src.model.training.base_training_strategy import BaseTrainingStrategy
 from src.model.evaluate_model import evaluate_model
 from src.model.visualize_results import visualize_results
+
+
+def r2_score(y_true, y_pred):
+    SS_res = tf.reduce_sum(tf.square(y_true - y_pred))
+    SS_tot = tf.reduce_sum(tf.square(y_true - tf.reduce_mean(y_true)))
+    return (1 - SS_res / (SS_tot + tf.keras.backend.epsilon()))
 
 
 class RandomSplitTrainingStrategy(BaseTrainingStrategy):
@@ -35,4 +42,4 @@ class RandomSplitTrainingStrategy(BaseTrainingStrategy):
         visualize_results(y_test.values, predictions, comet)
         logging.info(evaluate_model(y_test.values, predictions))
 
-#%%
+# %%
