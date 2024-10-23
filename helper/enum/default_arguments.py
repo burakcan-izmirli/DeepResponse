8c20c7b5e4501521d15f8131aa742ed58f12bbf3
burakcan-izmirli <burakcanizmirli@gmail.com>
2024-10-23 22:51:50 +0300

    feat(DRS-22): updating default arguments

--- helper/enum/default_arguments.py
+++ helper/enum/default_arguments.py
@@ -6,6 +6,8 @@ class DefaultArguments(Enum):
     batch_size: int = 64
     epoch: int = 50
     learning_rate: float = 0.001
-    data_type: str = 'pathway'
+    data_source: str 'depmap'
+    data_type: str = 'normal'
     split_type: str = 'random'
+    learning_task: str = 'classification'
     comet: bool = False
