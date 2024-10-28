f6959fe7aff6bf8c939e7b77fbddc77b15a7fce6
burakcan-izmirli <burakcanizmirli@gmail.com>
2024-10-28 12:45:12 +0300

    feat(DSE-22): refactoring

--- helper/enum/default_arguments.py
+++ helper/enum/default_arguments.py
@@ -6,7 +6,7 @@ class DefaultArguments(Enum):
     batch_size: int = 64
     epoch: int = 50
     learning_rate: float = 0.001
-    data_source: str 'depmap'
+    data_source: str = 'depmap'
     data_type: str = 'normal'
     split_type: str = 'random'
     learning_task: str = 'classification'
