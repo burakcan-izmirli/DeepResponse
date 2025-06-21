3186e4fbb25206968e9192cd1c511937d3f29803
burakcan-izmirli <burakcanizmirli@gmail.com>
2025-06-21 21:33:30 +0200

    feat(DRS-24): updating argument parser and default values

--- helper/argument_parser.py
+++ helper/argument_parser.py
@@ -1,33 +1,35 @@
 """ Argument parser """
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-
 from helper.enum.default_arguments import DefaultArguments
 
 
 def argument_parser():
-    """ Argument parser """
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-uc", "--use_comet", default=False, type=bool, help="Whether to use comet or not")
-    parser.add_argument('-lt', "--learning_task", default=DefaultArguments.learning_task.value, type=str, help="['classification', 'regression']")
-    parser.add_argument("-ds", "--data_source", default=DefaultArguments.data_source.value, type=str, help="['depmap', 'gdsc', 'ccle', 'nci_60']")
-    parser.add_argument("-es", "--evaluation_source", default=None, help="['depmap', 'gdsc', 'ccle', 'nci_60']")
+
+    parser.add_argument("-uc", "--use_comet", default=DefaultArguments.comet.value,
+                        type=lambda x: (str(x).lower() == 'true'), help="Whether to use comet or not")
+    parser.add_argument('-lt', "--learning_task", default=DefaultArguments.learning_task.value,
+                        type=str, help="['classification', 'regression']")
+    parser.add_argument("-ds", "--data_source", default=DefaultArguments.data_source.value,
+                        type=str, help="['depmap', 'gdsc', 'ccle', 'nci_60']")
+    parser.add_argument("-es", "--evaluation_source", default=None,
+                        type=str, help="['depmap', 'gdsc', 'ccle', 'nci_60']")
     parser.add_argument("-dt", "--data_type", default=DefaultArguments.data_type.value, type=str,
-                        help="For all data sources: ['normal', 'l1000', 'l1000_cross_domain] "
+                        help="For all data sources: ['normal', 'l1000', 'l1000_cross_domain'] "
                              "For just GDSC: ['pathway', 'pathway_reduced', 'digestive']")
     parser.add_argument("-st", "--split_type", default=DefaultArguments.split_type.value, type=str,
                         help="['random', 'cell_stratified', 'drug_stratified', 'drug_cell_stratified', 'cross_domain']")
-    parser.add_argument("-rs", "--random_state", default=DefaultArguments.random_state.value, type=int,
-                        help="Random State")
-    parser.add_argument("-bs", "--batch_size", default=DefaultArguments.batch_size.value, type=int, help="Batch Size")
-    parser.add_argument("-e", "--epoch", default=DefaultArguments.epoch.value, type=int, help="Epoch size")
-    parser.add_argument("-lr", "--learning_rate", default=DefaultArguments.learning_rate.value, type=float,
-                        help="Learning Rate")
+    parser.add_argument("-rs", "--random_state", default=DefaultArguments.random_state.value,
+                        type=int, help="Random state for reproducibility")
+    parser.add_argument("-bs", "--batch_size", default=DefaultArguments.batch_size.value,
+                        type=int, help="Batch size for training")
+    parser.add_argument("-e", "--epoch", default=DefaultArguments.epoch.value,
+                        type=int, help="Number of epochs for training")
+    parser.add_argument("-lr", "--learning_rate", default=DefaultArguments.learning_rate.value,
+                        type=float, help="Learning rate for optimizer")
+    parser.add_argument("-stl", "--selformer_trainable_layers",
+                        default=DefaultArguments.selformer_trainable_layers.value,
+                        type=int, help="Number of trainable layers in SELFormer")
 
-    args = vars(parser.parse_args())
-
-    # If evaluation source is not given then use data source as evaluation source.
-    if args["evaluation_source"] is None:
-        args["evaluation_source"] = args["data_source"]
-
-    return args["use_comet"], args["data_source"], args["evaluation_source"], args["data_type"], args["split_type"], \
-        args["random_state"], args["batch_size"], args["epoch"], args["learning_rate"], args["learning_task"]
+    args = parser.parse_args()
+    return args
--- helper/enum/default_arguments.py
+++ helper/enum/default_arguments.py
@@ -3,11 +3,12 @@ from enum import Enum
 
 class DefaultArguments(Enum):
     random_state: int = 12
-    batch_size: int = 64
-    epoch: int = 50
-    learning_rate: float = 0.001
+    batch_size: int = 32
+    epoch: int = 100
+    learning_rate: float = 0.0001
     data_source: str = 'depmap'
     data_type: str = 'normal'
     split_type: str = 'random'
-    learning_task: str = 'classification'
+    learning_task: str = 'regression'
     comet: bool = False
+    selformer_trainable_layers: int = -1
